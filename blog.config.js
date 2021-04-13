b4f8669426f1c1df6e10b6a42611b7f298009283
esorylul <esorylul@outlook.com>
2021-04-13 23:06:52 +0800

    Change appearance

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   link: 'https://life.tsiang.cc',
   description: '这是一个美好的时代.',
   lang: 'zh-CN',
-  appearance: 'light', // ['light', 'dark', 'auto'],
+  appearance: 'auto', // ['light', 'dark', 'auto'],
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#111827', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
